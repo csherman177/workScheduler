3eed9d3276a3d3faa5585a2498b41aa798f45382
Courtney Sherman <csherman177@gmail.com>
2023-03-27 21:14:58 -0600

    adding readMe file

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# workScheduler
+## Description 
+Create a simple calendar application that allows a user to save events for each hour of the day by modifying starter code. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+
+## Project URL
+  https://github.com/csherman177/workScheduler
+
+## Deployment
+  https://csherman177.github.io/workScheduler/ 
+
+## Demo/Screenshots
+  <table>
+  <tr>
+    <td>Password Generator Screenshot</td>
+  </tr>
+  <tr>
+    <td><img src= "workDayScheduler.png" height=300 alt="screenshot of workday scheduler"></td>
+  </tr>
+  </table>
+ 
+  ## Contact
+  Email: csherman177@gmail.com
+
+  ## Author
+  Author(s): Courtney Sherman 
+  GitHub: https://github.com/csherman177/ 
--- script.js
+++ script.js
@@ -47,16 +47,21 @@ function timeBlockColor() {
 // Button Click Needs to Store Information
 
 var textDescriptionEl = $("#col-8 col-md-10 description");
-var buttonEl = $("#btn saveBtn col-2 col-md-1");
-var inputAnswer = $(".3");
+
+//var inputAnswer = $(".3");
+//var timeEl = $("hour-9, hour-10, hour-11, hour-12, hour-12, hour-14, hour-15, hour-16, hour-17, hour-18")
+//var valueEl = ("fas fa-save")
+
 
 function saveToStorage () {
-  const value = inputAnswer.value;
-  localStorage.setItem("3",value);
-  console.log(localStorage)
+  const value = $(this).siblings('textarea').val()
+  const key = $(this).parent().attr("id");
+  localStorage.setItem(key,value);
 
-  buttonEl.addEventListener("click", saveToStorage);
 }
+$("#hour textarea").val(localStorage.getItem('hour'))
+
+$('.saveBtn').on("click", saveToStorage);
 
 // Call Functions displayTime & displayDate
 displayDate ();
@@ -68,32 +73,7 @@ timeBlockColor();
 
 $(function () {
 
-  
-  // Event Listener for Each Button
 
 
-
-  // Save Answers to Local Storage
-
-
-  
-
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+ 
 });
